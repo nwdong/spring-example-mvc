258f7390161eea867ff47417f4291aac2a7d6faf
nwdong <nwdong@gmail.com>
2017-08-31 11:14:16 +1000

    new skeleton based on Spring Boot

---
+++ src/main/java/spring/example/mvc/SpringExampleMvcApplication.java
@@ -0,0 +1,12 @@
+package spring.example.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExampleMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExampleMvcApplication.class, args);
+	}
+}
